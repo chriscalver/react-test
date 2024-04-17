34c340105c009488924deeff294c224ad8cab483
chriscalver <130410868+chriscalver@users.noreply.github.com>
2024-04-17 16:55:08 -0400

    test

---
+++ src/Home.js
@@ -0,0 +1,20 @@
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
+
+const Home = () => {
+  const {
+    error,
+    isPending,
+    data: blogs,
+  } = useFetch("https://www.chriscalver.com/react-test/data/db.json");
+
+  return (
+    <div className="home">
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} />}
+    </div>
+  );
+};
+
+export default Home;
